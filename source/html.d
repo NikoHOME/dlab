d8480f7a5ab18d6d13e1cf8abf826b21941e3326
NikoHOME <wiktor.gawronski@gmail.com>
2023-03-13 13:35:02 +0100

    Add basic layout

--- source/html.d
+++ source/html.d
@@ -46,25 +46,25 @@ immutable navbarDOC = elem!"nav"(
 						),
 					),
 					elem!"a"(
-						attr("href") = "/kontakt",
+						attr("href") = "/#Kontakt",
 						elem!"li"(
 							"Kontakt"
 						),
 					),
 					elem!"a"(
-						attr("href") = "#homeMinecraft",
+						attr("href") = "/#homeMinecraft",
 						elem!"li"(
 							"Minecraft"
 						),
 					),
 					elem!"a"(
-						attr("href") = "#homeKolonie",
+						attr("href") = "/#homeKolonie",
 						elem!"li"(
 							"Kolonie"
 						),
 					),
 					elem!"a"(
-						attr("href") = "#homeStacjonarne",
+						attr("href") = "/#homeStacjonarne",
 						elem!"li"(
 							"Stacjonarne"
 						),
@@ -119,17 +119,17 @@ immutable navbarDOC = elem!"nav"(
 				attr("class") = "dropdown-content",
 				elem!"a"(
 					attr("class") = "anim1",
-					attr("href") = "#homeMinecraft",
+					attr("href") = "/#homeMinecraft",
 					"Minecraft",
 				),
 				elem!"a"(
 					attr("class") = "anim2",
-					attr("href") = "#homeKolonie",
+					attr("href") = "/#homeKolonie",
 					"Kolonie",
 				),
 				elem!"a"(
 					attr("class") = "anim1",
-					attr("href") = "#homeStacjonarne",
+					attr("href") = "/#homeStacjonarne",
 					"Stacjonarne",
 				),
 				
@@ -139,7 +139,7 @@ immutable navbarDOC = elem!"nav"(
 		),
 		elem!"a"(
 			attr("class") = ["a-nav", "a-nav-theme"],
-			attr("href") = "/kontakt",
+			attr("href") = "/#Kontakt",
 			"Kontakt"
 		),
 	)
@@ -152,6 +152,7 @@ immutable headDOC = elem!"head"(
 	elemLink("stylesheet", "/static/css/style-nav.css"),
     elemLink("stylesheet", "/static/css/style-home.css"),
 	elemLink("stylesheet", "/static/css/style-footer.css"),
+	elemLink("stylesheet", "/static/css/style-separator.css"),
 	elemLink("stylesheet", "/static/css/style-offer.css"),
 	elemLink("stylesheet", "/static/css/style-dropdown.css"),
 	elemLink("stylesheet", "/static/css/style-burger.css"),
@@ -172,27 +173,54 @@ immutable webHome = text(
             navbarDOC,
             header("Lanlab"),
 
-            elem!"div"(
-                attr("class") = ["banner" ,"banner-test"],
-                elem!"img"(
-					attr("class") = "test",
-                    attr("src") = "static/img/test.png",
-                    attr("alt") = "logo"
-                )
-            ),
-		
 			elem!"div"(
-				attr("id") = "Oferty",
+				attr("class") = "home-banner",
+				elem!"div"(attr("class") = "home-anchor1"),
+				elem!"div"(attr("class") = "home-anchor2"),
+				elem!"div"(attr("class") = "home-anchor3"),
+				elem!"div"(
+					attr("class") = "home-column1",
+					elem!"img"(
+						attr("src") = "static/img/logo.png",
+						attr("alt") = "logo"
+                	),
+				),
+				elem!"div"(
+					attr("class") = "home-column2",
+					elem!"img"(
+						attr("src") = "static/img/screenshot.png",
+						attr("alt") = "logo"
+                	),				
+				),
+				elem!"div"(
+					attr("class") = "home-column3",
+					elem!"img"(
+						attr("src") = "static/img/favicon.png",
+						attr("alt") = "logo"
+                	),				
+				),
+			),
+			elem!"div"(
+				elem!"a"(
+					attr("class") = "anchor",
+					attr("id") = "Oferty",
+				),
 				elem!"h1"(
 					attr("class") = "offer-h1",
 					"Oferty"
 				),	
 				elem!"div"(
 					attr("class") = ["offer", "offer-right"],
-					attr("id") = "homeMinecraft",
-					elem!"img"(
-						attr("src") = "/static/img/blank.png",
-						attr("alt") = "blank"
+					elem!"a"(
+						attr("class") = "anchor",
+						attr("id") = "homeMinecraft",
+					),
+					elem!"div"(
+						attr("class") = "offer-img",
+						elem!"img"(
+							attr("src") = "/static/img/screenshot.png",
+							attr("alt") = "blank"
+						),
 					),
 					elem!"div"(
 						attr("class") = "offer-text",
@@ -223,10 +251,16 @@ immutable webHome = text(
 				),
 				elem!"div"(
 					attr("class") = ["offer", "offer-left"],
-					attr("id") = "homeKolonie",
-					elem!"img"(
-						attr("src") = "/static/img/blank.png",
-						attr("alt") = "blank"
+					elem!"a"(
+						attr("class") = "anchor",
+						attr("id") = "homeKolonie",
+					),
+					elem!"div"(
+						attr("class") = "offer-img",
+						elem!"img"(
+							attr("src") = "/static/img/screenshot.png",
+							attr("alt") = "blank"
+						),
 					),
 					elem!"div"(
 						attr("class") = "offer-text",
@@ -257,10 +291,16 @@ immutable webHome = text(
 				),
 				elem!"div"(
 					attr("class") = ["offer", "offer-right"],
-					attr("id") = "homeStacjonarne",
-					elem!"img"(
-						attr("src") = "/static/img/blank.png",
-						attr("alt") = "blank"
+					elem!"a"(
+						attr("class") = "anchor",
+						attr("id") = "homeStacjonarne",
+					),
+					elem!"div"(
+						attr("class") = "offer-img",
+						elem!"img"(
+							attr("src") = "/static/img/screenshot.png",
+							attr("alt") = "blank"
+						),
 					),
 					elem!"div"(
 						attr("class") = "offer-text",
@@ -380,14 +420,49 @@ immutable webHome = text(
 
 			elem!"div"(
 				attr("class") = "footer",
-				elem!"div"(
-					attr("class") = "footer-c",
+				elem!"a"(
+					attr("class") = "anchor",
+					attr("id") = "Kontakt",
 				),
 				elem!"div"(
 					attr("class") = "footer-c",
+					elem!"h1"(
+						"Language Laboratoris•"
+					),
 				),
 				elem!"div"(
 					attr("class") = "footer-c",
+					elem!"svg"(
+						attr("class") = "svg-icon",
+						attr("xmlns") = "http://www.w3.org/2000/svg",
+						attr("viewBox") = "0 0 384 512",
+						elem!"path"( 
+							attr("d") = "M0 64C0 28.7 28.7 0 64 0H288c35.3 0 64 28.7 64 64V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V64zM208 448a32 32 0 1 0 -64 0 32 32 0 1 0 64 0zM288 64H64V384H288V64z",
+						),
+					),
+					elem!"h3"(
+						"Zadzwoń"
+					),
+					elem!"h4"(
+						"533 011 544"
+					),
+				),
+				elem!"div"(
+					attr("class") = "footer-c",
+					elem!"svg"(
+						attr("class") = "svg-icon",
+						attr("xmlns") = "http://www.w3.org/2000/svg",
+						attr("viewBox") = "0 0 576 512",
+						elem!"path"(
+							attr("d") = "M408 120c0 54.6-73.1 151.9-105.2 192c-7.7 9.6-22 9.6-29.6 0C241.1 271.9 168 174.6 168 120C168 53.7 221.7 0 288 0s120 53.7 120 120zm8 80.4c3.5-6.9 6.7-13.8 9.6-20.6c.5-1.2 1-2.5 1.5-3.7l116-46.4C558.9 123.4 576 135 576 152V422.8c0 9.8-6 18.6-15.1 22.3L416 503V200.4zM137.6 138.3c2.4 14.1 7.2 28.3 12.8 41.5c2.9 6.8 6.1 13.7 9.6 20.6V451.8L32.9 502.7C17.1 509 0 497.4 0 480.4V209.6c0-9.8 6-18.6 15.1-22.3l122.6-49zM327.8 332c13.9-17.4 35.7-45.7 56.2-77V504.3L192 449.4V255c20.5 31.3 42.3 59.6 56.2 77c20.5 25.6 59.1 25.6 79.6 0zM288 152a40 40 0 1 0 0-80 40 40 0 1 0 0 80z",
+						),
+					),
+					elem!"h3"(
+						"Znajdź nas"
+					),
+					elem!"h4"(
+						"Hallera 201, Gdańsk"
+					),
 				),
 			),
 
