3e4bcfd0e4f517fbfea7b70a29ee0072f7571c5c
NikoHOME <wiktor.gawronski@gmail.com>
2023-05-14 21:15:58 +0200

    Add google manager

--- source/html.d
+++ source/html.d
@@ -152,6 +152,16 @@ immutable navbarDOC = elem!"nav"(
 );
 
 immutable headDOC = elem!"head"(
+
+	elemTrusted("
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-PSSBSCZ');</script>
+	<!-- End Google Tag Manager -->"),
+	
 	elem!"title"("Language Laboratories •"),
 	elemLink("icon", "/static/img/favicon.png"),
 	elemLink("stylesheet", "/static/css/master.css"),
